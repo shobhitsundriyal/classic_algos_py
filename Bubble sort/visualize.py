15b1964d6f5704d9826636097c838c45a882c5b2
unknown <shobhitsundriyal0170@gmail.com>
2020-03-09 22:07:33 +0530

    visualized

---
+++ Bubble sort/visualize.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from bubble_sort import bubble_sort
+import random
+import cv2
+import numpy as np
+import os
+import shutil
+
+num_element = int(input('Enter no of elements '))
+
+arr = list(range(num_element+1))
+f_arr = random.sample(arr, len(arr))
+
+generate = bubble_sort(f_arr)
+
+fig = plt.figure()
+
+
+i = 0
+for frame in generate:
+    i += 1
+    plt.bar(arr, height=f_arr)
+    plt.savefig('tmp/'+ 'frame' +'.png')
+    plt.clf()
+    cv2.imshow('Chalo', cv2.imread('tmp/frame.png'))
+    cv2.waitKey(1)
+
+
